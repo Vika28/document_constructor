f25c3ac03ab928d6f5cf56fd1fa2a43a3bb4fbef
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2023-04-06 15:59:57 +0200

    update readme, changed URL for making requests

--- frontend/src/URL.ts
+++ frontend/src/URL.ts
@@ -1,2 +1,2 @@
-export const url = 'http://localhost:5000';
-// export const url = 'http://documentconstructor-env.eba-e5h39a4x.eu-north-1.elasticbeanstalk.com';
+// export const url = 'http://localhost:5000';
+export const url = 'http://documentconstructor-env.eba-e5h39a4x.eu-north-1.elasticbeanstalk.com';
